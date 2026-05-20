alvinjt/server

feat(sharing): Init
Signed-off-by: provokateurin <kate@provokateurin.de>

apps/sharing/lib/Capabilities.php
@@ -1,0 +1,60 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace OCA\Sharing;
+
+use OCA\Sharing\AppInfo\Application;
+use OCP\Capabilities\ICapability;
+use OCP\Sharing\IRegistry;
+use OCP\Sharing\Permission\ISharePermissionCategory;
+
+final readonly class Capabilities implements ICapability {
+	public function __construct(
+		private IRegistry $registry,
+	) {
+	}
+
+	/**
+	 * @return array{
+	 *     sharing: array{
+	 *         api_versions: list<'v1'>,
+	 *         // Information for legacy compatibility with files_sharing.
+	 *         // As long as Unified Sharing is a translation layer to files_sharing this is needed.
+	 *         // As soon as this turns around and files_sharing becomes a translation layer for Unified Sharing,
+	 *         // this information will be removed.
+	 *         legacy?: array{
+	 *             // Maximum number of sources allowed to be selected by the user.
+	 *             max_sources: positive-int,
+	 *             // Maximum number of recipients allowed to be selected by the user.
+	 *             max_recipients: positive-int,
+	 *         },
+	 *         permission_categories: list<array{
+	 *             type: class-string<ISharePermissionCategory>,
+	 *             display_name: non-empty-string,
+	 *         }>,
+	 *     },
+	 * }
+	 */
+	#[\Override]
+	public function getCapabilities(): array {
+		return [
+			Application::APP_ID => [
+				'api_versions' => ['v1'],
+				'legacy' => [
+					'max_sources' => 1,
+					'max_recipients' => 1,
+				],
+				'permission_categories' => array_map(static fn (ISharePermissionCategory $permissionCategory): array => [
+					'type' => $permissionCategory::class,
+					'display_name' => $permissionCategory->getDisplayName(),
+				], array_values($this->registry->getPermissionCategories())),
+			],
+		];
+	}
+}

apps/sharing/lib/Migration/Version1000Date20250929161325.php
@@ -1,0 +1,67 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+
+namespace OCA\Sharing\Migration;
+
+use Closure;
+use Doctrine\DBAL\Schema\SchemaException;
+use OCP\DB\ISchemaWrapper;
+use OCP\DB\Types;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+use Override;
+
+final class Version1000Date20250929161325 extends SimpleMigrationStep {
+	/**
+	 * @param Closure():ISchemaWrapper $schemaClosure
+	 * @throws SchemaException
+	 */
+	#[Override]
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		$schema = $schemaClosure();
+
+		$shareTable = $schema->createTable('sharing_share');
+		$shareTable->addColumn('id', Types::BIGINT);
+		$shareTable->addColumn('owner', Types::TEXT);
+		$shareTable->addColumn('last_updated', Types::BIGINT);
+		$shareTable->addColumn('state', Types::TEXT);
+		$shareTable->setPrimaryKey(['id']);
+
+		$sourcesTable = $schema->createTable('sharing_share_sources');
+		$sourcesTable->addColumn('id', Types::BIGINT);
+		$sourcesTable->addColumn('source_type', Types::TEXT);
+		$sourcesTable->addColumn('source_value', Types::TEXT);
+		$sourcesTable->setPrimaryKey(['id', 'source_type', 'source_value']);
+		$sourcesTable->addForeignKeyConstraint($shareTable->getName(), ['id'], ['id'], ['onDelete' => 'CASCADE']);
+
+		$recipientsTable = $schema->createTable('sharing_share_recipients');
+		$recipientsTable->addColumn('id', Types::BIGINT);
+		$recipientsTable->addColumn('recipient_type', Types::TEXT);
+		$recipientsTable->addColumn('recipient_value', Types::TEXT);
+		$recipientsTable->setPrimaryKey(['id', 'recipient_type', 'recipient_value']);
+		$recipientsTable->addForeignKeyConstraint($shareTable->getName(), ['id'], ['id'], ['onDelete' => 'CASCADE']);
+
+		$propertiesTable = $schema->createTable('sharing_share_properties');
+		$propertiesTable->addColumn('id', Types::BIGINT);
+		$propertiesTable->addColumn('property_type', Types::TEXT);
+		$propertiesTable->addColumn('property_value', Types::TEXT, ['notnull' => false]);
+		$propertiesTable->setPrimaryKey(['id', 'property_type']);
+		$propertiesTable->addForeignKeyConstraint($shareTable->getName(), ['id'], ['id'], ['onDelete' => 'CASCADE']);
+
+		$permissionsTable = $schema->createTable('sharing_share_permissions');
+		$permissionsTable->addColumn('id', Types::BIGINT);
+		$permissionsTable->addColumn('permission_type', Types::TEXT);
+		$permissionsTable->addColumn('permission_enabled', Types::BOOLEAN);
+		$permissionsTable->setPrimaryKey(['id', 'permission_type']);
+		$permissionsTable->addForeignKeyConstraint($shareTable->getName(), ['id'], ['id'], ['onDelete' => 'CASCADE']);
+
+		return $schema;
+	}
+}

deic-dk/nextcloud_server

apps/sharing/lib/Capabilities.php
@@ -1,0 +1,60 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+namespace OCA\Sharing;
+
+use OCA\Sharing\AppInfo\Application;
+use OCP\Capabilities\ICapability;
+use OCP\Sharing\IRegistry;
+use OCP\Sharing\Permission\ISharePermissionCategoryType;
+
+final readonly class Capabilities implements ICapability {
+	public function __construct(
+		private IRegistry $registry,
+	) {
+	}
+
+	/**
+	 * @return array{
+	 *     sharing: array{
+	 *         api_versions: list<'v1'>,
+	 *         // Information for legacy compatibility with files_sharing.
+	 *         // As long as Unified Sharing is a translation layer to files_sharing this is needed.
+	 *         // As soon as this turns around and files_sharing becomes a translation layer for Unified Sharing,
+	 *         // this information will be removed.
+	 *         legacy?: array{
+	 *             // Maximum number of sources allowed to be selected by the user.
+	 *             max_sources: positive-int,
+	 *             // Maximum number of recipients allowed to be selected by the user.
+	 *             max_recipients: positive-int,
+	 *         },
+	 *         permission_categories: list<array{
+	 *             class: class-string<ISharePermissionCategoryType>,
+	 *             display_name: non-empty-string,
+	 *         }>,
+	 *     },
+	 * }
+	 */
+	#[\Override]
+	public function getCapabilities(): array {
+		return [
+			Application::APP_ID => [
+				'api_versions' => ['v1'],
+				'legacy' => [
+					'max_sources' => 1,
+					'max_recipients' => 1,
+				],
+				'permission_categories' => array_map(static fn (ISharePermissionCategoryType $permissionCategoryType): array => [
+					'class' => $permissionCategoryType::class,
+					'display_name' => $permissionCategoryType->getDisplayName(),
+				], array_values($this->registry->getPermissionCategoryTypes())),
+			],
+		];
+	}
+}

apps/sharing/lib/Migration/Version1000Date20250929161325.php
@@ -1,0 +1,74 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare(strict_types=1);
+
+
+namespace OCA\Sharing\Migration;
+
+use Closure;
+use Doctrine\DBAL\Schema\SchemaException;
+use OCP\DB\ISchemaWrapper;
+use OCP\DB\Types;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+use Override;
+
+final class Version1000Date20250929161325 extends SimpleMigrationStep {
+	/**
+	 * @param Closure():ISchemaWrapper $schemaClosure
+	 * @throws SchemaException
+	 */
+	#[Override]
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		$schema = $schemaClosure();
+
+		$shareTable = $schema->createTable('sharing_share');
+		$shareTable->addColumn('id', Types::BIGINT);
+		$shareTable->addColumn('owner', Types::TEXT);
+		$shareTable->addColumn('instance', Types::TEXT, ['notnull' => false]);
+		$shareTable->addColumn('last_updated', Types::BIGINT);
+		$shareTable->addColumn('state', Types::TEXT);
+		$shareTable->setPrimaryKey(['id']);
+
+		$sourcesTable = $schema->createTable('sharing_share_sources');
+		$sourcesTable->addColumn('share_id', Types::BIGINT);
+		$sourcesTable->addColumn('source_class', Types::TEXT);
+		$sourcesTable->addColumn('source_value', Types::TEXT);
+		$sourcesTable->setPrimaryKey(['share_id', 'source_class', 'source_value']);
+		$sourcesTable->addForeignKeyConstraint($shareTable->getName(), ['share_id'], ['id'], ['onDelete' => 'CASCADE']);
+
+		// TODO: Add possibility to mask permissions for recipients. For reshares the user may only mask permissions for their child recipients, not their self recipients
+		$recipientsTable = $schema->createTable('sharing_share_recipients');
+		$recipientsTable->addColumn('id', Types::BIGINT);
+		$recipientsTable->addColumn('parent_id', Types::BIGINT, ['notnull' => false]);
+		$recipientsTable->addColumn('share_id', Types::BIGINT);
+		$recipientsTable->addColumn('recipient_class', Types::TEXT);
+		$recipientsTable->addColumn('recipient_value', Types::TEXT);
+		$recipientsTable->addColumn('recipient_instance', Types::TEXT, ['notnull' => false]);
+		$recipientsTable->setPrimaryKey(['id']);
+		$recipientsTable->addUniqueIndex(['share_id', 'recipient_class', 'recipient_value']);
+		$recipientsTable->addForeignKeyConstraint($shareTable->getName(), ['share_id'], ['id'], ['onDelete' => 'CASCADE']);
+		$recipientsTable->addForeignKeyConstraint($recipientsTable->getName(), ['parent_id'], ['id'], ['onDelete' => 'CASCADE']);
+
+		$propertiesTable = $schema->createTable('sharing_share_properties');
+		$propertiesTable->addColumn('share_id', Types::BIGINT);
+		$propertiesTable->addColumn('property_class', Types::TEXT);
+		$propertiesTable->addColumn('property_value', Types::TEXT, ['notnull' => false]);
+		$propertiesTable->setPrimaryKey(['share_id', 'property_class']);
+		$propertiesTable->addForeignKeyConstraint($shareTable->getName(), ['share_id'], ['id'], ['onDelete' => 'CASCADE']);
+
+		$permissionsTable = $schema->createTable('sharing_share_permissions');
+		$permissionsTable->addColumn('share_id', Types::BIGINT);
+		$permissionsTable->addColumn('permission_class', Types::TEXT);
+		$permissionsTable->addColumn('permission_enabled', Types::BOOLEAN);
+		$permissionsTable->setPrimaryKey(['share_id', 'permission_class']);
+		$permissionsTable->addForeignKeyConstraint($shareTable->getName(), ['share_id'], ['id'], ['onDelete' => 'CASCADE']);
+
+		return $schema;
+	}
+}